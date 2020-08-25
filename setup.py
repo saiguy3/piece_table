73bbe6849b7c9034ca52bd5bdfba56fc980c0f74
saiguy3 <saiatmakuri@yahoo.com>
2020-08-24 22:45:33 -0700

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.rst", "r") as f:
 
 setup(
     name="piece_table",
-    version="0.0.2",
+    version="0.0.3",
     description="A Python implementation of the piece table data structure",
     long_description=long_description,
     long_description_content_type="text/x-rst",
